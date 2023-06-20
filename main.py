709076c832d37404082ae06219ad34184ae5c495
Jonathan <adoojonathan412@gmail.com>
2023-06-20 21:23:56 +0000

    typos

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 from bs4 import BeautifulSoup
 import requests
 
-html_text = request.get('https://www.timesjobs.com/candidate/job-search.html?searchType=personalizedSearch&from=submit&txtKeywords=python&txtLocation=').text
+html_text = requests.get('https://www.timesjobs.com/candidate/job-search.html?searchType=personalizedSearch&from=submit&txtKeywords=python&txtLocation=').text
 soup = BeautifulSoup(html_text, 'lxml')
-jobs = soup.find_all('li', class_ = 'clearfix job-bx wht-shd-bx')
+job = soup.find_all('li', class_ = 'clearfix job-bx wht-shd-bx')
 
-company_name = jobs.find('h3', class_ = 'joblist-comp-name').text.replace(' ', '')
-skills = jobs.find('span', class_ = 'srp-skills').text.replace(' ', '')
+company_name = job.find('h3', class_ = 'joblist-comp-name').text.replace(' ', '')
+skills = job.find('span', class_ = 'srp-skills').text.replace(' ', '')
 more_info = job.header.h2.a['href']
 
 print(f'Company Name: {company_name.strip()}')
