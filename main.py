5332de4ac33eb43243ffba95ff108116bad6a3c1
Jonathan <adoojonathan412@gmail.com>
2023-06-21 08:05:28 +0000

    added a functionality to display jobs which were posted recently

--- main.py
+++ main.py
@@ -4,11 +4,13 @@ import requests
 html_text = requests.get('https://www.timesjobs.com/candidate/job-search.html?searchType=personalizedSearch&from=submit&txtKeywords=python&txtLocation=').text
 soup = BeautifulSoup(html_text, 'lxml')
 job = soup.find_all('li', class_ = 'clearfix job-bx wht-shd-bx')
+published_date = job.find('span', class_ = 'sim-posted').span.text
 
-company_name = job.find('h3', class_ = 'joblist-comp-name').text.replace(' ', '')
-skills = job.find('span', class_ = 'srp-skills').text.replace(' ', '')
-more_info = job.header.h2.a['href']
+if 'few' in published_date:
+    company_name = job.find('h3', class_ = 'joblist-comp-name').text.replace(' ', '')
+    skills = job.find('span', class_ = 'srp-skills').text.replace(' ', '')
+    more_info = job.header.h2.a['href']
 
-print(f'Company Name: {company_name.strip()}')
-print(f'Skills Required: {skills.strip()}')
-print(f'More Information: {more_info}')
+    print(f'Company Name: {company_name.strip()}')
+    print(f'Skills Required: {skills.strip()}')
+    print(f'More Information: {more_info}')
