247f8ba32b11776e9d64f1a45747ae42023d04c9
Jonathan <adoojonathan412@gmail.com>
2023-06-21 08:21:56 +0000

    created a function to keep the whole program

--- main.py
+++ main.py
@@ -5,17 +5,20 @@ print('Put an unfamiliar skill')
 unfamiliar_skill = input('>')
 print(f'Filtering out {unfamiliar_skill}')
 
-html_text = requests.get('https://www.timesjobs.com/candidate/job-search.html?searchType=personalizedSearch&from=submit&txtKeywords=python&txtLocation=').text
-soup = BeautifulSoup(html_text, 'lxml')
-job = soup.find_all('li', class_ = 'clearfix job-bx wht-shd-bx')
-published_date = job.find('span', class_ = 'sim-posted').span.text
+def find_jobs():
+    html_text = requests.get('https://www.timesjobs.com/candidate/job-search.html?searchType=personalizedSearch&from=submit&txtKeywords=python&txtLocation=').text
+    soup = BeautifulSoup(html_text, 'lxml')
+    jobs = soup.find_all('li', class_ = 'clearfix job-bx wht-shd-bx')
+    
+    for job in jobs:
+        published_date = job.find('span', class_ = 'sim-posted').span.text
 
-if 'few' in published_date:
-    company_name = job.find('h3', class_ = 'joblist-comp-name').text.replace(' ', '')
-    skills = job.find('span', class_ = 'srp-skills').text.replace(' ', '')
-    more_info = job.header.h2.a['href']
+        if 'few' in published_date:
+            company_name = job.find('h3', class_ = 'joblist-comp-name').text.replace(' ', '')
+            skills = job.find('span', class_ = 'srp-skills').text.replace(' ', '')
+            more_info = job.header.h2.a['href']
 
-    if unfamiliar_skill not in skills:
-        print(f'Company Name: {company_name.strip()}')
-        print(f'Skills Required: {skills.strip()}')
-        print(f'More Information: {more_info}')
+            if unfamiliar_skill not in skills:
+                print(f'Company Name: {company_name.strip()}')
+                print(f'Skills Required: {skills.strip()}')
+                print(f'More Information: {more_info}')
